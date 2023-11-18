7d8fc9c271f9448b08bafb9f028ca9a41e28206c
prit-1543 <144490341+prit-1543@users.noreply.github.com>
2023-11-17 17:33:26 -0700

    Lab4

--- ToDoForm.jsx
+++ ToDoForm.jsx
@@ -1,13 +1,45 @@
 import React from 'react';
-import { View, TextInput, Button } from 'react-native';
+import {StyleSheet} from 'react-native';
+import {
+  View,
+  TextInput,
+  Button
+} from 'react-native';
+function ToDoForm({onAddTask}) {
+    const [taskText, setTaskText] = React.useState('');
 
-const ToDoForm = () => {
-  return (
-    <View>
-      <TextInput placeholder="Add new task..." />
-      <Button title="Add" onPress={() => {/* Add task handling logic here */}} />
-    </View>
-  );
-};
+    return (
+      <>
+       <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Add a new task..."
+          onChangeText={(text) => setTaskText(text)}
+          value={taskText}
+        />
+        <Button title="Add" onPress={() => onAddTask(taskText)}/>
+      </View>
+      </>
+    );
+}
+
+const styles = StyleSheet.create({
+  form: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginHorizontal: 20,
+    marginTop: 20,
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    marginRight: 10,
+  },
+});
+
 
 export default ToDoForm;
